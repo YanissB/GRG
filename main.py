bf688dc255a5b16d746e3aeafe5026acc3ece666
YanissB <91958136+YanissB@users.noreply.github.com>
2022-06-02 11:17:58 +0200

    adding interface w/ tkinter

--- main.py
+++ main.py
@@ -1,17 +1,63 @@
 import random
+from tkinter import *
+from tkinter.font import Font
 
-def generate_rhythm ():
+
+### Rhythm
+
+def generate_rhythm (label):
     pattern_rhythm = ""
     
     for eight_note in range(8):
         random_number = random.randint(1,2)
         if random_number == 1:
-            pattern_rythm += " __  "
+            pattern_rhythm += " __  "
         elif eight_note % 2 == 0:
-            pattern_rythm += "DOWN "
+            pattern_rhythm += "DOWN "
         else:
-            pattern_rythm += " UP  "
+            pattern_rhythm += " UP  "
     
-    return pattern_rhythm
+    label.config(text= pattern_rhythm)
 
-print("Try this: ", generate_rhythm())
+
+### Window configuration
+
+window = Tk()
+window.title("Random Guitar Rhythm Generator")
+window.geometry("1280x720")
+window.minsize(720, 480)
+window.iconbitmap("re_logo2.ico")
+window.config(background="#FFF7F6")
+
+
+### Add elements to the window
+
+## Frame
+
+frame_text = Frame(window, bg = "#FFF7F6", bd= 1)
+
+## TEXT
+label_title = Label(frame_text, text = "Welcome to RGRG", font=("Helvetica", 42), bg = "#FFF7F6")
+label_subtitle = Label(frame_text, text = "Click on the button to generate a rhythm pattern", font=("Helvetica", 25), bg = "#FFF7F6" )
+label_rhythm_pattern = Label(window, font=("Helvetica", 25), bg = "#FFF7F6" )
+
+## BUTTON
+
+generate_button = Button(window, text="LET'S GET FONKY", font=("Helvetica", 20), bg = "#000000", fg= "#FFF7F6", command= lambda :generate_rhythm(label_rhythm_pattern))
+
+
+## DISPLAY
+
+label_title.pack()
+label_subtitle.pack()
+frame_text.pack(expand=YES)
+
+generate_button.pack(expand=YES)
+label_rhythm_pattern.pack(expand=YES)
+
+
+window.mainloop()
+
+
+
+
